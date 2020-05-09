122842daf5a32f81d45960f5022c243972c2a98f
joaquin lema <joaco.lema@hotmail.com>
2020-05-09 17:27:26 -0300

    agregamos las referencias

--- src/webparts/initialWbpartSp/components/InitialWbpartSp.tsx
+++ src/webparts/initialWbpartSp/components/InitialWbpartSp.tsx
@@ -1,20 +1,24 @@
 import * as React from 'react';
-import styles from './InitialWbpartSp.module.scss';
+//import styles from './InitialWbpartSp.module.scss';
 import { IInitialWbpartSpProps } from './IInitialWbpartSpProps';
 import { escape } from '@microsoft/sp-lodash-subset';
+
+import { ISpfxReactCrudState } from './ISpfxReactCrudState'; 
+import { IListItem } from './IListItem';  
+import { SPHttpClient, SPHttpClientResponse } from '@microsoft/sp-http';
 
 export default class InitialWbpartSp extends React.Component<IInitialWbpartSpProps, {}> {
   public render(): React.ReactElement<IInitialWbpartSpProps> {
     return (
-      <div className={ styles.initialWbpartSp }>
-        <div className={ styles.container }>
-          <div className={ styles.row }>
-            <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
-              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
-              <p className={ styles.description }>{escape(this.props.description)}</p>
-              <a href="https://aka.ms/spfx" className={ styles.button }>
-                <span className={ styles.label }>Learn more</span>
+      <div >
+        <div >
+          <div >
+            <div >
+              <span >Welcome to SharePoint!</span>
+              <p >Customize SharePoint experiences using Web Parts.</p>
+              <p >{escape(this.props.description)}</p>
+              <a href="https://aka.ms/spfx" >
+                <span >Learn more</span>
               </a>
             </div>
           </div>
