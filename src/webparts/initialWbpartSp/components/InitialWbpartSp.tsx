8f34b24301318565255d0fd41fa4392e531da1c2
joaquin lema <joaco.lema@hotmail.com>
2020-05-09 17:29:28 -0300

    agregramos constructor

--- src/webparts/initialWbpartSp/components/InitialWbpartSp.tsx
+++ src/webparts/initialWbpartSp/components/InitialWbpartSp.tsx
@@ -8,6 +8,16 @@ import { IListItem } from './IListItem';
 import { SPHttpClient, SPHttpClientResponse } from '@microsoft/sp-http';
 
 export default class InitialWbpartSp extends React.Component<IInitialWbpartSpProps, {}> {
+  
+  constructor(props: IInitialWbpartSpProps, state: ISpfxReactCrudState) {  
+    super(props);  
+  
+    this.state = {  
+      status: 'Ready',  
+      items: []
+    }; 
+  }
+  
   public render(): React.ReactElement<IInitialWbpartSpProps> {
     return (
       <div >
