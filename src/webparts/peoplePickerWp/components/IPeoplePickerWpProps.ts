f4bd6095c92976bd46bc75cedd02bd9e7d2507c0
joaquin lema <joaco.lema@hotmail.com>
2020-05-09 19:47:43 -0300

    agregamos webpart de peoplepicker

---
+++ src/webparts/peoplePickerWp/components/IPeoplePickerWpProps.ts
@@ -0,0 +1,3 @@
+export interface IPeoplePickerWpProps {
+  description: string;
+}
---
+++ src/webparts/peoplePickerWp/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IPeoplePickerWpWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'PeoplePickerWpWebPartStrings' {
+  const strings: IPeoplePickerWpWebPartStrings;
+  export = strings;
+}
