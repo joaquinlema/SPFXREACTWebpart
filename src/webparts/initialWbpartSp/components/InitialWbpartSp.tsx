dcf9714c44198b8b2c0de62ad3267635facf4484
joaquin lema <joaco.lema@hotmail.com>
2020-05-09 18:55:37 -0300

    agregamos la implementacion de los metodos CRUD

--- src/webparts/initialWbpartSp/components/InitialWbpartSp.tsx
+++ src/webparts/initialWbpartSp/components/InitialWbpartSp.tsx
@@ -74,15 +74,216 @@ export default class InitialWbpartSp extends React.Component<IInitialWbpartSpPro
   }
 
   private createItem(): void {  
+    this.setState({  
+      status: 'Creating item...',  
+      items: []  
+    });  
+    
+    const body: string = JSON.stringify({  
+      'Title': `Item ${new Date()}`  
+    });  
+    
+    this.props.spHttpClient.post(`${this.props.siteUrl}/_api/web/lists/getbytitle('${this.props.listName}')/items`,  
+    SPHttpClient.configurations.v1,  
+    {  
+      headers: {  
+        'Accept': 'application/json;odata=nometadata',  
+        'Content-type': 'application/json;odata=nometadata',  
+        'odata-version': ''  
+      },  
+      body: body  
+    })  
+    .then((response: SPHttpClientResponse): Promise<IListItem> => {  
+      return response.json();  
+    })  
+    .then((item: IListItem): void => {  
+      this.setState({  
+        status: `Item '${item.Title}' (ID: ${item.Id}) successfully created`,  
+        items: []  
+      });  
+    }, (error: any): void => {  
+      this.setState({  
+        status: 'Error while creating the item: ' + error,  
+        items: []  
+      });  
+    });  
   }  
   
   private readItem(): void {  
+    this.setState({  
+      status: 'Loading latest items...',  
+      items: []  
+    });  
+    
+    this.getLatestItemId()  
+      .then((itemId: number): Promise<SPHttpClientResponse> => {  
+        if (itemId === -1) {  
+          throw new Error('No items found in the list');  
+        }  
+    
+        this.setState({  
+          status: `Loading information about item ID: ${itemId}...`,  
+          items: []  
+        });  
+        return this.props.spHttpClient.get(`${this.props.siteUrl}/_api/web/lists/getbytitle('${this.props.listName}')/items(${itemId})?$select=Title,Id`,  
+          SPHttpClient.configurations.v1,  
+          {  
+            headers: {  
+              'Accept': 'application/json;odata=nometadata',  
+              'odata-version': ''  
+            }  
+          });  
+      })  
+      .then((response: SPHttpClientResponse): Promise<IListItem> => {  
+        return response.json();  
+      })  
+      .then((item: IListItem): void => {  
+        this.setState({  
+          status: `Item ID: ${item.Id}, Title: ${item.Title}`,  
+          items: []  
+        });  
+      }, (error: any): void => {  
+        this.setState({  
+          status: 'Loading latest item failed with error: ' + error,  
+          items: []  
+        });  
+      });  
   }  
   
   private updateItem(): void {  
+    this.setState({  
+      status: 'Loading latest items...',  
+      items: []  
+    });  
+    
+    let latestItemId: number = undefined;  
+    
+    this.getLatestItemId()  
+      .then((itemId: number): Promise<SPHttpClientResponse> => {  
+        if (itemId === -1) {  
+          throw new Error('No items found in the list');  
+        }  
+    
+        latestItemId = itemId;  
+        this.setState({  
+          status: `Loading information about item ID: ${latestItemId}...`,  
+          items: []  
+        });  
+          
+        return this.props.spHttpClient.get(`${this.props.siteUrl}/_api/web/lists/getbytitle('${this.props.listName}')/items(${latestItemId})?$select=Title,Id`,  
+          SPHttpClient.configurations.v1,  
+          {  
+            headers: {  
+              'Accept': 'application/json;odata=nometadata',  
+              'odata-version': ''  
+            }  
+          });  
+      })  
+      .then((response: SPHttpClientResponse): Promise<IListItem> => {  
+        return response.json();  
+      })  
+      .then((item: IListItem): void => {  
+        this.setState({  
+          status: 'Loading latest items...',  
+          items: []  
+        });  
+    
+        const body: string = JSON.stringify({  
+          'Title': `Updated Item ${new Date()}`  
+        });  
+    
+        this.props.spHttpClient.post(`${this.props.siteUrl}/_api/web/lists/getbytitle('${this.props.listName}')/items(${item.Id})`,  
+          SPHttpClient.configurations.v1,  
+          {  
+            headers: {  
+              'Accept': 'application/json;odata=nometadata',  
+              'Content-type': 'application/json;odata=nometadata',  
+              'odata-version': '',  
+              'IF-MATCH': '*',  
+              'X-HTTP-Method': 'MERGE'  
+            },  
+            body: body  
+          })  
+          .then((response: SPHttpClientResponse): void => {  
+            this.setState({  
+              status: `Item with ID: ${latestItemId} successfully updated`,  
+              items: []  
+            });  
+          }, (error: any): void => {  
+            this.setState({  
+              status: `Error updating item: ${error}`,  
+              items: []  
+            });  
+          });  
+      });  
   }  
   
   private deleteItem(): void {  
+    if (!window.confirm('Are you sure you want to delete the latest item?')) {  
+      return;  
+    }  
+    
+    this.setState({  
+      status: 'Loading latest items...',  
+      items: []  
+    });  
+    
+    let latestItemId: number = undefined;  
+    let etag: string = undefined;  
+    this.getLatestItemId()  
+      .then((itemId: number): Promise<SPHttpClientResponse> => {  
+        if (itemId === -1) {  
+          throw new Error('No items found in the list');  
+        }  
+    
+        latestItemId = itemId;  
+        this.setState({  
+          status: `Loading information about item ID: ${latestItemId}...`,  
+          items: []  
+        });  
+    
+        return this.props.spHttpClient.get(`${this.props.siteUrl}/_api/web/lists/getbytitle('${this.props.listName}')/items(${latestItemId})?$select=Id`,  
+          SPHttpClient.configurations.v1,  
+          {  
+            headers: {  
+              'Accept': 'application/json;odata=nometadata',  
+              'odata-version': ''  
+            }  
+          });  
+      })  
+      .then((response: SPHttpClientResponse): Promise<IListItem> => {  
+        etag = response.headers.get('ETag');  
+        return response.json();  
+      })  
+      .then((item: IListItem): Promise<SPHttpClientResponse> => {  
+        this.setState({  
+          status: `Deleting item with ID: ${latestItemId}...`,  
+          items: []  
+        });  
+    
+        return this.props.spHttpClient.post(`${this.props.siteUrl}/_api/web/lists/getbytitle('${this.props.listName}')/items(${item.Id})`,  
+          SPHttpClient.configurations.v1,  
+          {  
+            headers: {  
+              'Accept': 'application/json;odata=nometadata',  
+              'Content-type': 'application/json;odata=verbose',  
+              'odata-version': '',  
+              'IF-MATCH': etag,  
+              'X-HTTP-Method': 'DELETE'  
+            }  
+          });  
+      })  
+      .then((response: SPHttpClientResponse): void => {  
+        this.setState({  
+          status: `Item with ID: ${latestItemId} successfully deleted`,  
+          items: []  
+        });  
+      }, (error: any): void => {  
+        this.setState({  
+          status: `Error deleting item: ${error}`,  
+          items: []  
+        });  
+      });  
   }  
 
   private getLatestItemId(): Promise<number> {  
