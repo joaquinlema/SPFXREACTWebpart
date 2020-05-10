cbb6e3360902d3a375d5b082c2904135071e995f
joaquin lema <joaco.lema@hotmail.com>
2020-05-09 21:38:59 -0300

    agregamos grid layout de prueba

--- src/webparts/peoplePickerWp/components/PeoplePickerWp.tsx
+++ src/webparts/peoplePickerWp/components/PeoplePickerWp.tsx
@@ -4,8 +4,9 @@ import { IPeoplePickerWpProps } from './IPeoplePickerWpProps';
 import { IPnPPeoplePickerState } from './PnPPeoplePickerState';
 import { escape } from '@microsoft/sp-lodash-subset';
 import { PeoplePicker, PrincipalType } from "@pnp/spfx-controls-react/lib/PeoplePicker";  
-// @pnp/sp imports    
-	
+import { GridLayout } from "@pnp/spfx-controls-react/lib/GridLayout";
+
+// @pnp/sp imports    	
 import { sp } from "@pnp/sp";
 import "@pnp/sp/webs";
 import "@pnp/sp/lists";
@@ -16,13 +17,62 @@ import { getGUID } from "@pnp/common";
 import { DefaultButton } from 'office-ui-fabric-react/lib/Button';   
 import { autobind } from 'office-ui-fabric-react';   
 
+import {
+  DocumentCard,
+  DocumentCardActivity,
+  DocumentCardPreview,
+  DocumentCardDetails,
+  DocumentCardTitle,
+  IDocumentCardPreviewProps,
+  DocumentCardLocation,
+  DocumentCardType
+} from 'office-ui-fabric-react/lib/DocumentCard';
+import { ImageFit } from 'office-ui-fabric-react/lib/Image';
+import { ISize } from 'office-ui-fabric-react/lib/Utilities';
+
 export default class PeoplePickerWp extends React.Component<IPeoplePickerWpProps, IPnPPeoplePickerState> {
   
   constructor(props: IPeoplePickerWpProps, state: IPnPPeoplePickerState) {    
     super(props);    
     
     this.state = {    
-      addUsers: []    
+      addUsers: [],
+      gridItems: [{
+        thumbnail: "https://pixabay.com/get/57e9dd474952a414f1dc8460825668204022dfe05555754d742e7bd6/hot-air-balloons-1984308_640.jpg",
+        title: "Adventures in SPFx",
+        name: "Perry Losselyong",
+        profileImageSrc: "https://robohash.org/blanditiisadlabore.png?size=50x50&set=set1",
+        location: "SharePoint",
+        activity: "3/13/2019"
+      }, {
+        thumbnail: "https://pixabay.com/get/55e8d5474a52ad14f1dc8460825668204022dfe05555754d742d79d0/autumn-3804001_640.jpg",
+        title: "The Wild, Untold Story of SharePoint!",
+        name: "Ebonee Gallyhaock",
+        profileImageSrc: "https://robohash.org/delectusetcorporis.bmp?size=50x50&set=set1",
+        location: "SharePoint",
+        activity: "6/29/2019"
+      }, {
+        thumbnail: "https://pixabay.com/get/57e8dd454c50ac14f1dc8460825668204022dfe05555754d742c72d7/log-cabin-1886620_640.jpg",
+        title: "Low Code Solutions: PowerApps",
+        name: "Seward Keith",
+        profileImageSrc: "https://robohash.org/asperioresautquasi.jpg?size=50x50&set=set1",
+        location: "PowerApps",
+        activity: "12/31/2018"
+      }, {
+        thumbnail: "https://pixabay.com/get/55e3d445495aa514f1dc8460825668204022dfe05555754d742b7dd5/portrait-3316389_640.jpg",
+        title: "Not Your Grandpa's SharePoint",
+        name: "Sharona Selkirk",
+        profileImageSrc: "https://robohash.org/velnammolestiae.png?size=50x50&set=set1",
+        location: "SharePoint",
+        activity: "11/20/2018"
+      }, {
+        thumbnail: "https://pixabay.com/get/57e6dd474352ae14f1dc8460825668204022dfe05555754d742a7ed1/faucet-1684902_640.jpg",
+        title: "Get with the Flow",
+        name: "Boyce Batstone",
+        profileImageSrc: "https://robohash.org/nulladistinctiomollitia.jpg?size=50x50&set=set1",
+        location: "Flow",
+        activity: "5/26/2019"
+      }]
     };    
   }    
 
@@ -49,12 +99,21 @@ export default class PeoplePickerWp extends React.Component<IPeoplePickerWpProps
                 principalTypes={[PrincipalType.User]}    
                 resolveDelay={1000} 
                 />  
+
               <DefaultButton    
                 data-automation-id="addSelectedUsers"    
                 title="Add Selected Users"    
                 onClick={this.addSelectedUsers}>    
                 Add Selected Users    
               </DefaultButton>    
+              
+              <br></br>
+
+              <GridLayout
+                ariaLabel="List of content, use right and left arrow keys to navigate, arrow down to access details."
+                items={this.state.gridItems}
+                onRenderGridItem={(item: any, finalSize: ISize, isCompact: boolean) => this._onRenderGridItem(item, finalSize, isCompact)}
+              />
             </div>
           </div>
         </div>
@@ -80,4 +139,41 @@ export default class PeoplePickerWp extends React.Component<IPeoplePickerWpProps
         console.log(i);  
     });  
   }   
+
+  private _onRenderGridItem = (item: any, finalSize: ISize, isCompact: boolean): JSX.Element => {
+    const previewProps: IDocumentCardPreviewProps = {
+      previewImages: [
+        {
+          previewImageSrc: item.thumbnail,
+          imageFit: ImageFit.cover,
+          height: 130
+        }
+      ]
+    };
+
+    return <div
+      data-is-focusable={true}
+      role="listitem"
+      aria-label={item.title}
+    >
+      <DocumentCard
+        type={isCompact ? DocumentCardType.compact : DocumentCardType.normal}
+        onClick={(ev: React.SyntheticEvent<HTMLElement>) => alert("You clicked on a grid item")}
+
+      >
+        <DocumentCardPreview {...previewProps} />
+        {!isCompact && <DocumentCardLocation location={item.location} />}
+        <DocumentCardDetails>
+          <DocumentCardTitle
+            title={item.title}
+            shouldTruncate={true}
+          />
+          <DocumentCardActivity
+            activity={item.activity}
+            people={[{ name: item.name, profileImageSrc: item.profileImageSrc }]}
+          />
+        </DocumentCardDetails>
+      </DocumentCard>
+    </div>;
+  }
 }
--- src/webparts/peoplePickerWp/components/PnPPeoplePickerState.ts
+++ src/webparts/peoplePickerWp/components/PnPPeoplePickerState.ts
@@ -1,3 +1,4 @@
 export interface IPnPPeoplePickerState {  
     addUsers: string[];  
+    gridItems: any[];
 }  
