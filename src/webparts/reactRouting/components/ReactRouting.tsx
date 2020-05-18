cef640e0c6932182dc442d10bebcf3adfa07fd26
joaquin lema <joaco.lema@hotmail.com>
2020-05-18 19:55:31 -0300

    agregamos navbar

--- src/webparts/reactRouting/components/ReactRouting.tsx
+++ src/webparts/reactRouting/components/ReactRouting.tsx
@@ -1,28 +1,31 @@
 import * as React from 'react';
 import styles from './ReactRouting.module.scss';
 import { BrowserRouter as Router, HashRouter, Switch, Route } from 'react-router-dom';
-import NavBar from './navBar/NavBar';
+import NavBarOfficeUi from './navBar/NavBarOfficeUi';
 import formularioAuto from './pages/FormularioAuto';
 import About from './pages/About';
 import { Fragment } from 'react';
+import { Stack, StackItem } from 'office-ui-fabric-react';
 
 
 const ReactRouting = () => {
   return (
     <HashRouter>
-      <NavBar />
-      <div className={styles.container}>
-        <Switch>
-          <Route exact={true} path='/' render={props => (
-            <Fragment>
-              <h1>Hola soy la pagina inicial</h1>
-            </Fragment>
-          )} />
+      <Stack horizontal gap={15}>
+        <NavBarOfficeUi /> 
+        <StackItem grow={2}>
+          <Switch>
+            <Route exact={true} path='/' render={props => (
+              <Fragment>
+                <h1>Hola soy la pagina inicial</h1>
+              </Fragment>
+            )} />
 
-          <Route  path='/formularioAuto' component={formularioAuto} />
-          <Route  path='/about' component={About} />
-        </Switch>
-      </div>
+            <Route path='/formularioAuto' component={formularioAuto} />
+            <Route path='/about' component={About} />
+          </Switch>
+        </StackItem>
+      </Stack>
     </HashRouter>
   );
 }
